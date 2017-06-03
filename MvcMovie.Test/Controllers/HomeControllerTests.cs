0b1f69f1b5528e0a9ceeebc4714b6fe246e52522
Ratan Sunder Parai <ratanparai@gmail.com>
2017-06-03 15:48:51 +0600

    Change project structure for TDD

---
+++ MvcMovie.Test/Controllers/HomeControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+using MvcMovie.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Test.Controllers
+{
+    public class HomeControllerTests
+    {
+        private readonly HomeController _homeController;
+        public HomeControllerTests() 
+        {
+            _homeController = new HomeController();
+        }
+
+        [Fact]
+        public void Index()
+        {
+            var result = _homeController.Index() as IActionResult;
+            Assert.NotNull(result);
+
+        }
+
+        [Fact]
+        public void About() 
+        {
+            var result = _homeController.About() as ViewResult;
+            Assert.Equal("Your application description page." , result.ViewData["Message"]) ;
+        }
+
+    }
+}
---
+++ MvcMovie.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace MvcMovie.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+
+        }
+    }
+}
